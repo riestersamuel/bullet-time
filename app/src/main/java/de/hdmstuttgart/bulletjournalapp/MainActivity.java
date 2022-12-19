74ab4912fe462ba56becf060db5d2f3171c2aefe
Samuel Riester <riestersamuel@icloud.com>
2022-12-19 19:29:41 +0100

    Fixed a small bug in the nav bar

--- app/src/main/java/de/hdmstuttgart/bulletjournalapp/MainActivity.java
+++ app/src/main/java/de/hdmstuttgart/bulletjournalapp/MainActivity.java
@@ -1,8 +1,11 @@
 package de.hdmstuttgart.bulletjournalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
