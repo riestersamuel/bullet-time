4c77a87ea5641b39d5b3233f1374ea26e8f9ca6f
Samuel Riester <riestersamuel@icloud.com>
2022-12-22 12:08:21 +0100

    Finished the basic UI of the timer and added simple OnClickListeners for the buttons

--- app/src/main/java/de/hdmstuttgart/bulletjournalapp/TimerFragment.java
+++ app/src/main/java/de/hdmstuttgart/bulletjournalapp/TimerFragment.java
@@ -1,18 +1,24 @@
 package de.hdmstuttgart.bulletjournalapp;
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link TimerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class TimerFragment extends Fragment {
 
 	// TODO: Rename parameter arguments, choose names that match
@@ -36,6 +42,7 @@ public class TimerFragment extends Fragment {
 	 * @param param2 Parameter 2.
 	 * @return A new instance of fragment ProfileFragment.
 	 */
+
 	// TODO: Rename and change types and number of parameters
 	public static TimerFragment newInstance(String param1, String param2) {
 		TimerFragment fragment = new TimerFragment();
@@ -53,12 +60,46 @@ public class TimerFragment extends Fragment {
 			mParam1 = getArguments().getString(ARG_PARAM1);
 			mParam2 = getArguments().getString(ARG_PARAM2);
 		}
+
+
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 							 Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
+		// Must be last line of this method
 		return inflater.inflate(R.layout.fragment_timer, container, false);
+	}
+
+	@Override
+	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+
+		// Defining the UI elements
+		ExtendedFloatingActionButton extended_fab_start = getView().findViewById(R.id.extended_fab_start);
+		ExtendedFloatingActionButton extended_fab_stop = getView().findViewById(R.id.extended_fab_stop);
+		FloatingActionButton small_fab_pause = getView().findViewById(R.id.small_fab_pause);
+		TextView information_text = getView().findViewById(R.id.information_text);
+		TextView remaining_time = getView().findViewById(R.id.remaining_time);
+
+		// Adding ClickListeners to the buttons
+		extended_fab_start.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				extended_fab_start.setVisibility(View.INVISIBLE);
+				extended_fab_stop.setVisibility(View.VISIBLE);
+				small_fab_pause.setVisibility(View.VISIBLE);
+			}
+		});
+
+		extended_fab_stop.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				extended_fab_start.setVisibility(View.VISIBLE);
+				extended_fab_stop.setVisibility(View.INVISIBLE);
+				small_fab_pause.setVisibility(View.INVISIBLE);
+			}
+		});
 	}
 }
