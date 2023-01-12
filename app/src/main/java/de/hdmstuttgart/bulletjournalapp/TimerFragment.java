ada827bc11fbcf4175b13c5990d4ce715fcbb092
Samuel Riester <riestersamuel@icloud.com>
2023-01-12 11:19:59 +0100

    Added instructions (a link to a video) to the timer.

--- app/src/main/java/de/hdmstuttgart/bulletjournalapp/TimerFragment.java
+++ app/src/main/java/de/hdmstuttgart/bulletjournalapp/TimerFragment.java
@@ -1,5 +1,8 @@
 package de.hdmstuttgart.bulletjournalapp;
+
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,9 +11,11 @@ import androidx.fragment.app.Fragment;
 
 import android.os.CountDownTimer;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -25,208 +30,219 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class TimerFragment extends Fragment {
 
-	CountDownTimer timer;
-	CountDownTimer shortBreakTimer;
-	CountDownTimer longBreakTimer;
+    CountDownTimer timer;
+    CountDownTimer shortBreakTimer;
+    CountDownTimer longBreakTimer;
 
-	// TODO: Rename parameter arguments, choose names that match
-	// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-	private static final String ARG_PARAM1 = "param1";
-	private static final String ARG_PARAM2 = "param2";
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
-	// TODO: Rename and change types of parameters
-	private String mParam1;
-	private String mParam2;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
 
-	public TimerFragment() {
-		// Required empty public constructor
-	}
+    public TimerFragment() {
+        // Required empty public constructor
+    }
 
-	/**
-	 * Use this factory method to create a new instance of
-	 * this fragment using the provided parameters.
-	 *
-	 * @param param1 Parameter 1.
-	 * @param param2 Parameter 2.
-	 * @return A new instance of fragment ProfileFragment.
-	 */
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProfileFragment.
+     */
 
-	// TODO: Rename and change types and number of parameters
-	public static TimerFragment newInstance(String param1, String param2) {
-		TimerFragment fragment = new TimerFragment();
-		Bundle args = new Bundle();
-		args.putString(ARG_PARAM1, param1);
-		args.putString(ARG_PARAM2, param2);
-		fragment.setArguments(args);
-		return fragment;
-	}
+    // TODO: Rename and change types and number of parameters
+    public static TimerFragment newInstance(String param1, String param2) {
+        TimerFragment fragment = new TimerFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		if (getArguments() != null) {
-			mParam1 = getArguments().getString(ARG_PARAM1);
-			mParam2 = getArguments().getString(ARG_PARAM2);
-		}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
 
 
-	}
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-							 Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		View view = inflater.inflate(R.layout.fragment_timer, container, false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_timer, container, false);
 
-		// Get a reference to the MaterialToolbar view
-		MaterialToolbar topBar = view.findViewById(R.id.topAppBar);
+        // Get a reference to the MaterialToolbar view
+        MaterialToolbar topBar = view.findViewById(R.id.topAppBar);
 
-		// Set the title for the MaterialToolbar
-		topBar.setTitle("Pomodoro Timer");
+        // Set the title for the MaterialToolbar
+        topBar.setTitle("Pomodoro Timer");
 
-		return view;
-	}
+        return view;
+    }
 
-	@Override
-	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-		super.onViewCreated(view, savedInstanceState);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-		// Defining the UI elements
-		ExtendedFloatingActionButton extended_fab_start = getView().findViewById(R.id.extended_fab_start);
-		ExtendedFloatingActionButton extended_fab_stop = getView().findViewById(R.id.extended_fab_stop);
-		TextView information_text = getView().findViewById(R.id.information_text);
-		TextView remaining_time = getView().findViewById(R.id.remaining_time);
-		remaining_time.setText("");
-		//information_text.setText("Focus session: Start a pomodoro timer and only focus on your most important task for the next 25 minutes. No distractions allowed! After that, take a 5 minute break and repeat the process. After four focussed sessions, take a longer break of 15 minutes and afterwards restart the process.");
-		information_text.setText("Currently no timer is running. Start a new timer or find out how this works by clicking on the question mark in the top right.");
+        // Defining the UI elements
+        ExtendedFloatingActionButton extended_fab_start = getView().findViewById(R.id.extended_fab_start);
+        ExtendedFloatingActionButton extended_fab_stop = getView().findViewById(R.id.extended_fab_stop);
+        TextView information_text = getView().findViewById(R.id.information_text);
+        TextView remaining_time = getView().findViewById(R.id.remaining_time);
+        remaining_time.setText("");
+        //information_text.setText("Focus session: Start a pomodoro timer and only focus on your most important task for the next 25 minutes. No distractions allowed! After that, take a 5 minute break and repeat the process. After four focussed sessions, take a longer break of 15 minutes and afterwards restart the process.");
+        information_text.setText("Currently no timer is running. Start a new timer or find out how this works by clicking on the question mark in the top right.");
 
-		shortBreakTimer = new CountDownTimer(5000, 1000) {
-			@Override
-			public void onTick(long millisUntilFinished) {
-				int remainingTime = (int) (millisUntilFinished / 60000 + 1);
-				remaining_time.setText("" + remainingTime);
-				information_text.setText("minutes of your short break remain. \nEnjoy the break!");
-			}
+        shortBreakTimer = new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int remainingTime = (int) (millisUntilFinished / 60000 + 1);
+                remaining_time.setText("" + remainingTime);
+                information_text.setText("minutes of your short break remain. \nEnjoy the break!");
+            }
 
-			@Override
-			public void onFinish() {
-				// Rufe die Funktion auf, die den Timer startet (also die Funktion, die der extended_fab_start onClickListener aufruft)
-				extended_fab_start.callOnClick();
-			}
-		};
+            @Override
+            public void onFinish() {
+                // Rufe die Funktion auf, die den Timer startet (also die Funktion, die der extended_fab_start onClickListener aufruft)
+                extended_fab_start.callOnClick();
+            }
+        };
 
-		longBreakTimer = new CountDownTimer(5000, 1000) {
-			@Override
-			public void onTick(long millisUntilFinished) {
-				int remainingTime = (int) (millisUntilFinished / 60000 + 1);
-				remaining_time.setText("" + remainingTime);
-				information_text.setText("minutes of your long break remain. Enjoy the break!");
-			}
+        longBreakTimer = new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int remainingTime = (int) (millisUntilFinished / 60000 + 1);
+                remaining_time.setText("" + remainingTime);
+                information_text.setText("minutes of your long break remain. Enjoy the break!");
+            }
 
-			@Override
-			public void onFinish() {
-				// Rufe die Funktion auf, die den Timer startet (also die Funktion, die der extended_fab_start onClickListener aufruft)
-				extended_fab_start.callOnClick();
-			}
-		};
+            @Override
+            public void onFinish() {
+                // Rufe die Funktion auf, die den Timer startet (also die Funktion, die der extended_fab_start onClickListener aufruft)
+                extended_fab_start.callOnClick();
+            }
+        };
 
-		// Our pomodoro timer, here set to 25 minutes (1500000 milliseconds)
-		// Every minute, change the UI (60000 milliseconds)
-		// TODO: Diese Klasse auslagern
-		timer = new CountDownTimer(5000, 1000) {
-			// The remaining minutes
-			int breakCounter = 0;
-			@Override
-			public void onTick(long millisUntilFinished) {
-				int remainingTime = (int) (millisUntilFinished / 60000 + 1);
-				remaining_time.setText("" + remainingTime);
-			}
+        // Our pomodoro timer, here set to 25 minutes (1500000 milliseconds)
+        // Every minute, change the UI (60000 milliseconds)
+        // TODO: Diese Klasse auslagern
+        timer = new CountDownTimer(5000, 1000) {
+            // The remaining minutes
+            int breakCounter = 0;
 
-			@Override
-			public void onFinish() {
-				// End the animation
-				ImageView clockAnimation = view.findViewById(R.id.clock_animation);
-				clockAnimation.setVisibility(View.INVISIBLE);
-				((AnimationDrawable) ((ImageView) view.findViewById(R.id.clock_animation)).getDrawable()).stop();
-				breakCounter++;
-				if (breakCounter % 4 == 0) {
-					longBreakTimer.start();
-					startAnimation();
-				}
-				else {
-					shortBreakTimer.start();
-					startAnimation();
-				}
-				}
-		};
+            @Override
+            public void onTick(long millisUntilFinished) {
+                int remainingTime = (int) (millisUntilFinished / 60000 + 1);
+                remaining_time.setText("" + remainingTime);
+            }
 
-		// Adding ClickListeners to the buttons
-		extended_fab_start.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				remaining_time.setText("25");
-				information_text.setText("minutes of your pomodoro session remain. \nFocus on your most important task!");
-				extended_fab_start.setVisibility(View.INVISIBLE);
-				extended_fab_stop.setVisibility(View.VISIBLE);
-				timer.start();
+            @Override
+            public void onFinish() {
+                // End the animation
+                ImageView clockAnimation = view.findViewById(R.id.clock_animation);
+                clockAnimation.setVisibility(View.INVISIBLE);
+                ((AnimationDrawable) ((ImageView) view.findViewById(R.id.clock_animation)).getDrawable()).stop();
+                breakCounter++;
+                if (breakCounter % 4 == 0) {
+                    longBreakTimer.start();
+                    startAnimation();
+                } else {
+                    shortBreakTimer.start();
+                    startAnimation();
+                }
+            }
+        };
 
-				// That's for the timer animation
-				// Get a reference to the ImageView
-				ImageView clockAnimation = view.findViewById(R.id.clock_animation);
-				clockAnimation.setVisibility(View.VISIBLE);
-				// Set the animation-list drawable as the src attribute
-				clockAnimation.setImageResource(R.drawable.clock_animation);
-				// Start the animation
-				((AnimationDrawable) clockAnimation.getDrawable()).start();
+        // Adding ClickListeners to the buttons
+        extended_fab_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                remaining_time.setText("25");
+                information_text.setText("minutes of your pomodoro session remain. \nFocus on your most important task!");
+                extended_fab_start.setVisibility(View.INVISIBLE);
+                extended_fab_stop.setVisibility(View.VISIBLE);
+                timer.start();
 
-				// Hide tomato image
-				ImageView tomato = view.findViewById(R.id.tomatoImage);
-				tomato.setVisibility(View.GONE);
-			}
-		});
+                // That's for the timer animation
+                // Get a reference to the ImageView
+                ImageView clockAnimation = view.findViewById(R.id.clock_animation);
+                clockAnimation.setVisibility(View.VISIBLE);
+                // Set the animation-list drawable as the src attribute
+                clockAnimation.setImageResource(R.drawable.clock_animation);
+                // Start the animation
+                ((AnimationDrawable) clockAnimation.getDrawable()).start();
 
-		extended_fab_stop.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				information_text.setText("You stopped the timer. Start a new one.");
-				extended_fab_start.setVisibility(View.VISIBLE);
-				extended_fab_stop.setVisibility(View.INVISIBLE);
-				timer.cancel();
-				shortBreakTimer.cancel();
-				longBreakTimer.cancel();
-				// Reset the remaining time
-				remaining_time.setText("");
-				// End the animation
-				stopAnimation();
-				// Hide tomato image
-				ImageView tomato = view.findViewById(R.id.tomatoImage);
-				tomato.setVisibility(View.GONE);
-			}
-		});
-	}
+                // Hide tomato image
+                ImageView tomato = view.findViewById(R.id.tomatoImage);
+                tomato.setVisibility(View.GONE);
+            }
+        });
 
-	private void startAnimation(){
-		// That's for the timer animation
-		View view = super.getView();
-		ImageView clockAnimation = view.findViewById(R.id.clock_animation);
-		clockAnimation.setVisibility(View.VISIBLE);
-		((AnimationDrawable) ((ImageView) view.findViewById(R.id.clock_animation)).getDrawable()).start();
-	}
+        extended_fab_stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                information_text.setText("You stopped the timer. Start a new one.");
+                extended_fab_start.setVisibility(View.VISIBLE);
+                extended_fab_stop.setVisibility(View.INVISIBLE);
+                timer.cancel();
+                shortBreakTimer.cancel();
+                longBreakTimer.cancel();
+                // Reset the remaining time
+                remaining_time.setText("");
+                // End the animation
+                stopAnimation();
+                // Hide tomato image
+                ImageView tomato = view.findViewById(R.id.tomatoImage);
+                tomato.setVisibility(View.GONE);
+            }
+        });
 
-	private void stopAnimation(){
-		// That's for the timer animation
-		View view = super.getView();
-		ImageView clockAnimation = view.findViewById(R.id.clock_animation);
-		clockAnimation.setVisibility(View.INVISIBLE);
-		((AnimationDrawable) ((ImageView) view.findViewById(R.id.clock_animation)).getDrawable()).stop();
-	}
+        // Linking the questionmark icon to the video
+        MaterialToolbar toolbar = (MaterialToolbar) view.findViewById(R.id.topAppBar);
+        toolbar.setOnMenuItemClickListener(item -> {
+            if (item.getItemId() == R.id.help) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=1pADI_eZ_-U"));
+                startActivity(browserIntent);
+                return true;
+            }
+            return false;
+        });
+    }
 
-	@Override
-	public void onPause() {
-		super.onPause();
-		// Stop the timer when the fragment is paused
-		if (timer!=null) timer.cancel();
-		if(shortBreakTimer!=null) shortBreakTimer.cancel();
-		if(longBreakTimer!=null) longBreakTimer.cancel();
-	}
+    private void startAnimation() {
+        // That's for the timer animation
+        View view = super.getView();
+        ImageView clockAnimation = view.findViewById(R.id.clock_animation);
+        clockAnimation.setVisibility(View.VISIBLE);
+        ((AnimationDrawable) ((ImageView) view.findViewById(R.id.clock_animation)).getDrawable()).start();
+    }
+
+    private void stopAnimation() {
+        // That's for the timer animation
+        View view = super.getView();
+        ImageView clockAnimation = view.findViewById(R.id.clock_animation);
+        clockAnimation.setVisibility(View.INVISIBLE);
+        ((AnimationDrawable) ((ImageView) view.findViewById(R.id.clock_animation)).getDrawable()).stop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Stop the timer when the fragment is paused
+        if (timer != null) timer.cancel();
+        if (shortBreakTimer != null) shortBreakTimer.cancel();
+        if (longBreakTimer != null) longBreakTimer.cancel();
+    }
 }
